386c15c41aa5cadd0196ec6237ead37a3b05083c
lolboysg <mateo.viel@epitech.eu>
2022-08-05 19:28:21 +0200

    fix: fetch the price of every contract using coingecko and not only PAL

--- src/scripts/getTotalPricePerToken.ts
+++ src/scripts/getTotalPricePerToken.ts
@@ -6,7 +6,7 @@ const getTotalPricePerToken = async (
   tokenAddress: string,
 ): Promise<number> => {
   const res = await axios.get(
-    'https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xab846fb6c81370327e784ae7cbb6d6a6af6ff4bf&vs_currencies=usd',
+    `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokenAddress}&vs_currencies=usd`,
   );
   const result = tokenAmount.toNumber() * res.data[tokenAddress].usd;
   return result;
