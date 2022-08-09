21f0ca00d708f4b6eb0cc915b50ffd2c9d2024be
lolboysg <mateo.viel@epitech.eu>
2022-08-09 23:45:37 +0200

    feat: specific protocol uri in the title embed link

--- src/scripts/getProtocolEmbed.ts
+++ src/scripts/getProtocolEmbed.ts
@@ -22,11 +22,12 @@ export const getProtocolEmbed = async (
   const totalRewardToken = getTotalRewardToken(objectiveVotes, rewardPerVote);
   const totalPrice = await getTotalPricePerToken(totalRewardToken, rewardToken);
   const protocolName = protocol === ProtocolType.Curve ? 'veCRV' : 'veBAL';
+  const protocolURI = protocol === ProtocolType.Curve ? 'protocol=crv' : 'protocol=bal';
 
   const exampleEmbed = {
     color: 0xfffff,
     title: `New ${protocolName} Quest: ${gaugeSymbol}`,
-    url: 'http://app.warden.vote/quest/',
+    url: `http://app.warden.vote/quest/?${protocolURI}`,
     description: `$${rewardTokenSymbol} rewards are now available on app.warden.vote\n\n${totalRewardToken.toString()} $${rewardTokenSymbol} ($${totalPrice.toFixed(
       3,
     )}) can be captured by users who vote for ${gaugeSymbol}`,
