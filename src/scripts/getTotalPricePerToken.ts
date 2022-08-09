f8aeb629dfe9bd9b61231e5633dbf14f2802ff83
lolboysg <mateo.viel@epitech.eu>
2022-08-10 00:38:07 +0200

    fix: lowercase the tokenAddress to use for coingecko

--- src/scripts/getTotalPricePerToken.ts
+++ src/scripts/getTotalPricePerToken.ts
@@ -6,6 +6,7 @@ const getTotalPricePerToken = async (
   tokenAddress: string,
 ): Promise<number> => {
   try {
+    tokenAddress = tokenAddress.toLowerCase();
     const res = await axios.get(
       `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokenAddress}&vs_currencies=usd`,
     );
