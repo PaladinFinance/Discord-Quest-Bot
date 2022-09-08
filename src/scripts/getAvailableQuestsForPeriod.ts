92af057baa1d926a0ecd4d3b8e1a5085f650f4c7
0xtekgrinder <0xtekgrinder@protonmail.com>
2022-09-08 19:52:49 +0200

    refactor: change week to seconds variable to a global variable

--- src/scripts/getAvailableQuestsForPeriod.ts
+++ src/scripts/getAvailableQuestsForPeriod.ts
@@ -1,13 +1,13 @@
 import { BigNumber, Contract } from 'ethers';
+import { WEEK } from 'src/globals/time';
 import provider from '../config/etherProvider';
 import QuestBoardAbi from '../data/abi/QuestBoardAbi.json';
 
 const getAvailableQuestsForPeriod = async (address: string): Promise<BigNumber> => {
   try {
     const contract = new Contract(address, QuestBoardAbi, provider);
-    const week = 7 * 24 * 60 * 60;
     const availableQuestsNb = await contract.getQuestIdsForPeriod(
-      BigNumber.from(Date.now()).div(1000).div(week).mul(week),
+      BigNumber.from(Date.now()).div(1000).div(WEEK).mul(WEEK),
     );
     return availableQuestsNb.length;
   } catch (err) {
