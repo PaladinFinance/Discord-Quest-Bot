38b3a47f8cd2fa0a2d22fa985e3f320b94fb1524
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2023-02-15 11:59:31 +0100

    Root gauges on alt chains (#14)
    * feat: now handles root gauges on alt chains
    
    * refactor: use only thegraph to get the symbol from gauge
    
    * fix: revert index.ts

--- src/scripts/getSymbolFromGauge.ts
+++ src/scripts/getSymbolFromGauge.ts
@@ -1,16 +1,84 @@
 import { Contract } from 'ethers';
-import provider from '../config/etherProvider';
-import gaugeAbi from '../data/abi/gaugeAbi.json';
+import etherProvider from '../config/etherProvider';
+import erc20 from '../data/abi/erc20.json';
+import gaugeRootAbi from '../data/abi/gaugeRootAbi.json';
+import axios from 'axios';
+
+type Chain = 'Polygon' | 'Optimism' | 'Arbitrum';
+
+const getSymbol = async (recipient: string, chain: Chain): Promise<string | undefined> => {
+  let res;
+  try {
+    res = await axios.post(
+      `https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gauges-${chain.toLowerCase()}`,
+      {
+        query: `{
+          liquidityGauges(where: {streamer: "${recipient}"}) {
+            symbol
+          }
+        }`,
+      },
+    );
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+  if (!res.data.data.liquidityGauges[0]) return;
+  return res.data.data.liquidityGauges[0].symbol;
+};
+
+const determineChain = async (gauge: string): Promise<Chain | undefined> => {
+  let res;
+  try {
+    res = await axios.post(
+      `https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gauges`,
+      {
+        query: `{
+          rootGauges(where: {id: "${gauge.toLowerCase()}"}) {
+            chain
+          }
+        }`,
+      },
+    );
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+  if (!res.data.data.rootGauges[0]) return;
+  return res.data.data.rootGauges[0].chain;
+};
+
+const getSymbolFromRootGauge = async (gauge: string): Promise<string | undefined> => {
+  // Get the pool address
+  let recipient: string;
+  try {
+    const gaugeRootContract = new Contract(gauge, gaugeRootAbi, etherProvider);
+    recipient = await gaugeRootContract.getRecipient();
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+
+  // Determine the chain
+  const chain = await determineChain(gauge);
+  if (!chain) return;
+
+  // Get the pool address
+  const symbol = await getSymbol(recipient, chain);
+  return symbol;
+};
 
 const getSymbolFromGauge = async (gauge: string): Promise<string> => {
+  let symbol: string = '';
+
   try {
-    const gaugeContract = new Contract(gauge, gaugeAbi, provider);
-    const symbol = await gaugeContract.symbol();
-    return symbol;
-  } catch (err) {
-    console.error(err);
-    return '';
+    const gaugeContract = new Contract(gauge, erc20, etherProvider);
+    symbol = await gaugeContract.symbol();
+  } catch (e) {
+    const expectedSymbol = await getSymbolFromRootGauge(gauge);
+    if (expectedSymbol) symbol = expectedSymbol;
   }
+  return symbol;
 };
 
 export default getSymbolFromGauge;
