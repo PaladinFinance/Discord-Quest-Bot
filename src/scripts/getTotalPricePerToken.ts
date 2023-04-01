990c4704714ec41c1c9de66cb39bd5b743662902
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-04-01 19:58:15 +0200

    feat: change the price api from congecko to defi llama to get more accurate pricing

--- src/scripts/getTotalPricePerToken.ts
+++ src/scripts/getTotalPricePerToken.ts
@@ -7,10 +7,8 @@ const getTotalPricePerToken = async (
 ): Promise<number> => {
   try {
     tokenAddress = tokenAddress.toLowerCase();
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokenAddress}&vs_currencies=usd`,
-    );
-    const result = tokenAmount.toNumber() * res.data[tokenAddress].usd;
+    const res = await axios.get(`https://coins.llama.fi/prices/current/ethereum:${tokenAddress}`);
+    const result = tokenAmount.toNumber() * res.data.coins[`ethereum:${tokenAddress}`].price;
     return result;
   } catch (err) {
     console.error(err);
