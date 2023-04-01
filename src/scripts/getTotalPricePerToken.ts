82bea4e9a19810a63ee662aeff42cb6b6334aca1
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-04-01 20:04:14 +0200

    feat: add a callback to coingecko when getting the price of a quest

--- src/scripts/getTotalPricePerToken.ts
+++ src/scripts/getTotalPricePerToken.ts
@@ -12,6 +12,15 @@ const getTotalPricePerToken = async (
     return result;
   } catch (err) {
     console.error(err);
+    try {
+      const res = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokenAddress}&vs_currencies=usd`,
+      );
+      const result = tokenAmount.toNumber() * res.data[tokenAddress].usd;
+      return result;
+    } catch (err2) {
+      console.error(err2);
+    }
     return 0;
   }
 };
