52404d972d4fc885d440f87f200d66ba258a017f
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-04-07 23:33:05 +0200

    refactor: improve the getTotalPrice to have a cleaner code

--- src/scripts/getTotalPricePerToken.ts
+++ src/scripts/getTotalPricePerToken.ts
@@ -1,28 +1,27 @@
 import axios from 'axios';
 import { BigNumber } from 'ethers';
 
+const getLlamaPrice = async (tokenAddress: string): Promise<number> => {
+  const res = await axios.get(`https://coins.llama.fi/prices/current/ethereum:${tokenAddress}`);
+  return res.data.coins[`ethereum:${tokenAddress}`].price;
+};
+
+const getCoingeckoPrice = async (tokenAddress: string): Promise<number> => {
+  const res = await axios.get(
+    `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokenAddress}&vs_currencies=usd`,
+  );
+  return res.data[tokenAddress].usd;
+};
+
 const getTotalPricePerToken = async (
   tokenAmount: BigNumber,
   tokenAddress: string,
 ): Promise<number> => {
-  try {
-    tokenAddress = tokenAddress.toLowerCase();
-    const res = await axios.get(`https://coins.llama.fi/prices/current/ethereum:${tokenAddress}`);
-    const result = tokenAmount.toNumber() * res.data.coins[`ethereum:${tokenAddress}`].price;
-    return result;
-  } catch (err) {
-    console.error(err);
-    try {
-      const res = await axios.get(
-        `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokenAddress}&vs_currencies=usd`,
-      );
-      const result = tokenAmount.toNumber() * res.data[tokenAddress].usd;
-      return result;
-    } catch (err2) {
-      console.error(err2);
-    }
-    return 0;
-  }
+  tokenAddress = tokenAddress.toLowerCase();
+  const tokenPrice = await getLlamaPrice(tokenAddress).catch(() =>
+    getCoingeckoPrice(tokenAddress).catch(() => 0),
+  );
+  return tokenPrice * tokenAmount.toNumber();
 };
 
 export default getTotalPricePerToken;
