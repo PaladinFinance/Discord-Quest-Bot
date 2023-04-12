7299cfc2cbae871272ad9db5b049cbca7a0adf8b
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2023-04-12 19:39:30 +0200

    feat: get the curve gauge symbol from a subgraph to get it wherever the chain

--- src/scripts/getSymbolFromGauge.ts
+++ src/scripts/getSymbolFromGauge.ts
@@ -5,7 +5,7 @@ import CurveGaugeAbi from '../data/abi/CurveGaugeAbi.json';
 import axios from 'axios';
 import { ProtocolType } from './getProtocolEmbed';
 
-const getSymbol = async (recipient: string, chain: string): Promise<string | undefined> => {
+const getBalancerSymbol = async (recipient: string, chain: string): Promise<string | undefined> => {
   let res;
   try {
     res = await axios.post(
@@ -22,11 +22,11 @@ const getSymbol = async (recipient: string, chain: string): Promise<string | und
     console.error(e);
     return;
   }
-  if (!res.data || !res.data.data || !res.data.data.liquidityGauges) return;
+  if (!res.data || !res.data.data || !res.data.data.liquidityGauges || res.data.data.liquidityGauges === 0) return;
   return res.data.data.liquidityGauges[0].symbol;
 };
 
-const determineChain = async (gauge: string): Promise<string | undefined> => {
+const determineBalancerChain = async (gauge: string): Promise<string | undefined> => {
   let res;
   try {
     res = await axios.post(
@@ -47,7 +47,7 @@ const determineChain = async (gauge: string): Promise<string | undefined> => {
   return res.data.data.rootGauge.chain;
 };
 
-const getSymbolFromLiquidityGauge = async (gauge: string): Promise<string | undefined> => {
+const getSymbolFromBalancerLiquidityGauge = async (gauge: string): Promise<string | undefined> => {
   let res;
   try {
     res = await axios.post(
@@ -68,7 +68,7 @@ const getSymbolFromLiquidityGauge = async (gauge: string): Promise<string | unde
   return res.data.data.liquidityGauge.symbol;
 };
 
-const getRecipient = async (gauge: string) => {
+const getBalancerRecipient = async (gauge: string) => {
   let res;
   try {
     res = await axios.post(
@@ -89,43 +89,59 @@ const getRecipient = async (gauge: string) => {
   return res.data.data.rootGauge.recipient;
 };
 
-const getSymbolFromRootGauge = async (gauge: string): Promise<string | undefined> => {
+const getSymbolFromBalancerRootGauge = async (gauge: string): Promise<string | undefined> => {
   // Get the pool address
-  const recipient = await getRecipient(gauge);
+  const recipient = await getBalancerRecipient(gauge);
   if (!recipient) return;
 
   // Determine the chain
-  const chain = await determineChain(gauge);
+  const chain = await determineBalancerChain(gauge);
   if (!chain) return;
 
   // Get the pool address
-  const symbol = await getSymbol(recipient, chain);
+  const symbol = await getBalancerSymbol(recipient, chain);
   return symbol;
 };
 
 const getSymbolFromBalancerGauge = async (gauge: string): Promise<string> => {
-  const mainetSymbol = await getSymbolFromLiquidityGauge(gauge);
+  const mainetSymbol = await getSymbolFromBalancerLiquidityGauge(gauge);
   if (mainetSymbol) return mainetSymbol;
 
-  const altSymbol = await getSymbolFromRootGauge(gauge);
+  const altSymbol = await getSymbolFromBalancerRootGauge(gauge);
   if (altSymbol) return altSymbol;
 
   return '';
 };
 
-const getSymbolFromCurveGauge = async (gauge: string): Promise<string> => {
-  let symbol: string = '';
-
+const getSymbolFromCurveLiquidityGauge = async (gauge: string, chain: string): Promise<string | undefined> => {
+  let res;
   try {
-    const gaugeContract = new Contract(gauge, CurveGaugeAbi, etherProvider);
-    const lpAddress = await gaugeContract.lp_token();
-
-    const lpContract = new Contract(lpAddress, ERC20, etherProvider);
-    symbol = await lpContract.symbol();
+    res = await axios.post(
+      `https://api.thegraph.com/subgraphs/name/messari/curve-finance-${chain}`,
+      {
+        query: `{
+          liquidityPools(where: {_gaugeAddress: "${gauge.toLowerCase()}}") {
+            symbol
+          }
+        }`,
+      },
+    );
   } catch (e) {
     console.error(e);
+    return;
   }
-  return symbol;
+  if (!res.data || !res.data.data || !res.data.data.liquidityPools || res.data.data.liquidityPools.length === 0) return;
+  return res.data.data.liquidityPools[0].symbol;
+}
+
+const getSymbolFromCurveGauge = async (gauge: string): Promise<string> => {
+  const chains = ["ethereum", "fantom", "polygon", "avalanche", "arbitrum", "harmony", "optimism", "gnosis", "aurora"];
+
+  for (const chain of chains) {
+    const symbol = getSymbolFromCurveLiquidityGauge(gauge, chain);
+    if (symbol) return symbol;
+  }
+  return '';
 };
 
 const getSymbolFromGauge = async (gauge: string, protocol: ProtocolType): Promise<string> => {
