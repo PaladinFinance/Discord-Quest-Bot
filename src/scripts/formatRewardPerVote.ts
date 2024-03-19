993ef08af970c1cbd619936e8385214593113de6
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-03-19 19:10:50 -0400

    style: apply prettier

--- src/scripts/formatRewardPerVote.ts
+++ src/scripts/formatRewardPerVote.ts
@@ -2,6 +2,6 @@ import { ethers } from 'ethers';
 
 const formatRewardPerVote = (rewardPerVote: bigint, decimals: bigint): string => {
   return ethers.formatUnits(rewardPerVote, decimals);
-}
+};
 
 export default formatRewardPerVote;
